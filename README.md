80ab65bc26460f2b305e8a785a811e60b71f990f
Mia Flores <flameprince338@gmail.com>
2018-02-20 13:28:20 +0000

    Update debate.js

--- README.md
+++ README.md
@@ -14,7 +14,6 @@ You will be given admin privileges on the bot that way.
 2) Jr Nice Guy
 3) sniper
 4) PowerHitter0418
-5) XnadrojX
 # Special Thanks
 1)  XnadrojX
 2)  Hoeenhero
---
+++ chat-plugins/poll.js
@@ -0,0 +1,8 @@
+'use strict';
+exports.commands = {
+    poll: function (target, room, user) {
+        target = target.split(',');
+        let q = target[0];
+        // will do this later
+    }
+};
