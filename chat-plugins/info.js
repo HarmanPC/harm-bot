e998ea0c5635466edffa5781b30e807fb5c92dd8
Mia Flores <flameprince338@gmail.com>
2018-03-13 12:57:35 +0000

    o_k

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -15,22 +15,10 @@ exports.commands = {
         let startTime = Date.now() - (process.uptime() * 1000);
         this.send("The bot's uptime is: " + Tools.getTimeAgo(startTime));
     },
-    help: function(target, room, user) {
-        if (!target) return this.parse("/guide");
-        target = target.toLowerCase();
-        this.can("say");
-        if (!Tools.helpEntries[target]) return false;
-        Tools.helpEntries[target].forEach(e => {
-            this.send(e.replace(/^\//i, room ? room.commandCharacter[0] : Config.defaultCharacter));
-        });
-    },
     guide: function(target, room, user) {
         this.can("set");
-        let useCommandCharacter = room ? room.commandCharacter[0] : Config.defaultCharacter[0];
-        let hastebin = Object.keys(Tools.helpEntries).sort().map(entry => Tools.helpEntries[entry].join("\n").replace(/^\//i, useCommandCharacter).replace(/\n\//i, useCommandCharacter)).join("\n\n");
-        Tools.uploadToHastebin("Bot Commands: \n\n" + hastebin, link => {
-            this.send("Bot Guide: " + link);
-        });
+        if (!Config.guide) return this.send('There is no guide.');
+        this.send('A guide on how to use ' + Monitor.username + ' can be found here: ' + Config.guide);
     },
     git: function(target, room, user) {
         this.can("set");
